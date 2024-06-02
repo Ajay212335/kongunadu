0d671ecd771c3b111b53c1d499de46931261fb17
Ajay212335 <ajaiks2005@gmail.com>
2024-06-03 01:49:45 +0530

    Create eb_lms.sql

---
+++ eb_lms.sql
@@ -0,0 +1,566 @@
+CREATE DATABASE user_registration;
+
+USE user_registration;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE DATABASE user_registration2;
+
+USE user_registration2;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE DATABASE user_registration3;
+
+USE user_registration3;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE DATABASE user_registration4;
+
+USE user_registration4;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE DATABASE user_registration5;
+
+USE user_registration5;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE DATABASE user_registration6;
+
+USE user_registration6;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE DATABASE user_registration7;
+
+USE user_registration7;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE DATABASE user_registration8;
+
+USE user_registration8;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE DATABASE user_registration9;
+
+USE user_registration9;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE DATABASE user_registration10;
+
+USE user_registration10;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE DATABASE user_registration11;
+
+USE user_registration11;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE DATABASE user_registration12;
+
+USE user_registration12;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE DATABASE user_registration13;
+
+USE user_registration13;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE userss (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    register_number VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    work VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    register TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE DATABASE login_register;
+
+USE login_register;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE DATABASE login_register2;
+
+USE login_register2;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE DATABASE login_register3;
+
+USE login_register3;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE DATABASE login_register4;
+
+USE login_register4;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE DATABASE login_register5;
+
+USE login_register5;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE DATABASE login_register6;
+
+USE login_register6;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE DATABASE login_register7;
+
+USE login_register7;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE DATABASE login_register8;
+
+USE login_register8;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+
+CREATE DATABASE login_register9;
+
+USE login_register9;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE DATABASE login_register10;
+
+USE login_register10;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE DATABASE login_register11;
+
+USE login_register11;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE DATABASE login_register12;
+
+USE login_register12;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE DATABASE login_register13;
+
+USE login_register13;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
